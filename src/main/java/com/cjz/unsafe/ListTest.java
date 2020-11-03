c25eeec9aa9a5196ed61af8cfe6ae3404367525e
ChenJingZhuo <3279838729@qq.com>
2020-11-03 19:22:32 +0800

    CopyOnWriteArrayList

---
+++ src/main/java/com/cjz/unsafe/ListTest.java
@@ -0,0 +1,36 @@
+package com.cjz.unsafe;
+
+import java.util.*;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+//学习方法：1、先会用；2、货比三家，寻找其他解决方法；3、分析源码！
+//java.util.ConcurrentModificationException 并发修改异常
+public class ListTest {
+
+    public static void main(String[] args) {
+        //并发下 ArrayList 不安全的
+        /**
+         * 解决方案
+         * 1、List<String> list = new Vector<>();
+         * 2、List<String> list = Collections.synchronizedList(new ArrayList<>());
+         * 3、List<String> list = new CopyOnWriteArrayList<>();
+         */
+        //List<String> list = new ArrayList<>();
+        //List<String> list = new Vector<>();
+        //List<String> list = Collections.synchronizedList(new ArrayList<>());
+
+
+        //CopyOnWrite 写入时复制 COW 计算机程序设计领域的一种优化策略
+        //多线程调用的时候，list，读取的时候，固定的，写入（覆盖）
+        //在写入的时候避免覆盖，造成数据问题！
+        //读写分离 CopyOnWriteArrayList 比 Vector NB 在哪里？
+        List<String> list = new CopyOnWriteArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            new Thread(()->{
+                list.add(UUID.randomUUID().toString().substring(0,5));
+                System.out.println(list);
+            },String.valueOf(i)).start();
+        }
+    }
+
+}
